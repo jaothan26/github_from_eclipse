af3e4ddd76fcc45d5499708c7f3d1cc114d0568a
jaothan26 <44733945+jaothan26@users.noreply.github.com>
2020-10-06 14:28:38 +0200

    Update App

--- github_from_eclipse/src/main/java/com/github_from_eclipse/App.java
+++ github_from_eclipse/src/main/java/com/github_from_eclipse/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Change made from Github" );
         System.out.println( "Hello World!" );
     }
 }
